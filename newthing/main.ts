3c1d412650da769d6e2320fd53e76391b67d694e
quinm0 <quin@punkorbit.space>
2025-06-24 23:07:42 -0400

    bun init

---
+++ newthing/main.ts
@@ -0,0 +1,32 @@
+import * as THREE from "three";
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  1,
+  500
+);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
+
+const scene = new THREE.Scene();
+
+// init scene and renderer and add to DOM
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+scene.background = new THREE.Color(0x000000);
+
+renderer.render(scene, camera);
+
+function drawBox(x: number, y: number) {
+  const geometry = new THREE.BoxGeometry(x, y);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+}
+
+drawBox(10, 10);
