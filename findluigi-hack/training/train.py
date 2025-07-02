6ae3d8da8b3b32be187347981eb5e6fb6c3cba5c
quinm0 <krwm3wttk@mozmail.com>
2025-07-02 18:12:57 -0400

    add test frame and update epochs

--- findluigi-hack/training/train.py
+++ findluigi-hack/training/train.py
@@ -5,11 +5,11 @@ from ultralytics import YOLO
 Reference the documentation for valid model specifications: https://docs.ultralytics.com/models
 '''
 # Use more epochs, lower batch size, and enable augmentation for small datasets
-my_data = "/home/qmoran/everything/findluigi-hack/training/dataset.yaml"  # path to your dataset.yaml file
-model = YOLO("/home/qmoran/everything/findluigi-hack/training/yolov8n.pt")
+my_data = "training/dataset.yaml"  # path to your dataset.yaml file
+model = YOLO("training/yolov8n.pt")
 results = model.train(
     data=my_data,
-    epochs=200,           # more epochs for small data
+    epochs=5,           # more epochs for small data (for testing training methods)
     batch=4,              # lower batch size
     imgsz=640,            # default image size
     lr0=0.01,             # slightly higher learning rate
