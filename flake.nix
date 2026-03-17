8e81be5537703d1a550a16ed841f00181d4e16cc
quinm0 <jolly2633@tutamail.com>
2026-03-17 16:36:03 -0400

    add nixos-hardware

--- flake.nix
+++ flake.nix
@@ -3,14 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }@inputs: {
     nixosConfigurations.qmoran-laptop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -19,6 +20,7 @@
         ./nix/user-quin.nix
         ./nix/gui1.nix
         ./nix/mega.nix
+        nixos-hardware.nixosModules.framework-11th-gen-intel
       ];
     };
 
