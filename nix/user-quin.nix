b33332e78681be606c97d7545db77b98b1d955b8
quinm0 <jolly2633@tutamail.com>
2026-03-20 14:52:14 -0400

    add steam to quin user

--- nix/user-quin.nix
+++ nix/user-quin.nix
@@ -25,4 +25,11 @@
       trash-cli
     ];
   };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 }
