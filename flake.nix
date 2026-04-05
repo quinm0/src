d29e73e6aaf3016c50517d3fbb0457c491141d88
quinm0 <jolly2633@tutamail.com>
2026-04-05 19:31:46 -0400

    so far nothing work :(

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
@@ -11,31 +12,42 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }@inputs: {
-    nixosConfigurations.qmoran-laptop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./nix/qlhc.nix
-        ./nix/common.nix
-        ./nix/user-quin.nix
-        ./nix/gui1.nix
-        ./nix/mega.nix
-        nixos-hardware.nixosModules.framework-11th-gen-intel
+  outputs = inputs@{ flake-parts, nixos-hardware, lib, ... }:
+    # https://flake.parts/module-arguments.html
+    flake-parts.lib.mkFlake { inherit inputs; } (top@{ config, withSystem, moduleWithSystem, ... }: {
+      imports = [
       ];
-    };
-
-    nixosConfigurations.qmoran-desktop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./nix/qdhc.nix
-        ./nix/common.nix
-        ./nix/user-quin.nix
-        ./nix/gui1.nix
-        ./nix/mega.nix
-        ./nix/jf-server.nix
+      flake = {
+        nixosConfigurations.qmoran-laptop = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./nix/qlhc.nix
+            ./nix/common.nix
+            ./nix/user-quin.nix
+            ./nix/gui1.nix
+            ./nix/mega.nix
+            nixos-hardware.nixosModules.framework-11th-gen-intel
+          ];
+        };
+        nixosConfigurations.qmoran-desktop = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./nix/qdhc.nix
+            ./nix/common.nix
+            ./nix/user-quin.nix
+            ./nix/gui1.nix
+            ./nix/mega.nix
+            ./nix/jf-server.nix
+          ];
+        };
+      };
+      systems = [
+        "x86_64-linux"
       ];
-    };
-
-  };
+      perSystem = { config, pkgs, ... }: {
+      };
+    });
 }
 
+
+    
