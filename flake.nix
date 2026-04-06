877677d79fd6ba7daff8e63d85aedb07cb28b4e6
quinm0 <jolly2633@tutamail.com>
2026-04-05 22:00:18 -0400

    import tree is cool

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
@@ -12,7 +13,7 @@
     };
   };
 
-  outputs = inputs@{ flake-parts, nixos-hardware, nixpkgs, ... }:
+  outputs = inputs@{ flake-parts, nixos-hardware, import-tree, nixpkgs, ... }:
     # https://flake.parts/module-arguments.html
     flake-parts.lib.mkFlake { inherit inputs; } (top@{ config, withSystem, moduleWithSystem, ... }: {
       imports = [
@@ -21,22 +22,16 @@
         nixosConfigurations.qmoran-laptop = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            ./nix/qlhc.nix
-            ./nix/common.nix
-            ./nix/user-quin.nix
-            ./nix/gui1.nix
-            ./nix/mega.nix
+            (import-tree ./modules)
             nixos-hardware.nixosModules.framework-11th-gen-intel
+            ./nix/qlhc.nix
           ];
         };
         nixosConfigurations.qmoran-desktop = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
+            (import-tree ./modules)
             ./nix/qdhc.nix
-            ./nix/common.nix
-            ./nix/user-quin.nix
-            ./nix/gui1.nix
-            ./nix/mega.nix
             ./nix/jf-server.nix
           ];
         };
