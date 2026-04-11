7e04f6a9368e18d1a4ac725bfab376fe81d5cd92
quinm0 <jolly2633@tutamail.com>
2026-04-11 11:35:08 -0400

    update filesystem mount(?)

--- nix/qdhc.nix
+++ nix/qdhc.nix
@@ -31,8 +31,12 @@
     };
 
   fileSystems."/mnt/tmpMedia" =
-    { device = "/dev/disk/by-uuid/fa20e116-e04e-4f3e-bf5a-c2e2c1fad610";
+    { 
+      device = "/dev/disk/by-uuid/fa20e116-e04e-4f3e-bf5a-c2e2c1fad610";
       fsType = "ext4";
+      options = ["noatime" "nodiratime" "noauto"];
+
+      systemd.automount = true;
     };
 
   swapDevices =
