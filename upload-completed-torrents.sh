614a90c4c4627e3385c451928a7c94f0ede91301
quinm0 <jolly2633@tutamail.com>
2026-04-11 11:41:37 -0400

    add move for sonarr folder too

--- upload-completed-torrents.sh
+++ upload-completed-torrents.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-sudo rclone move --config /etc/rclone.conf --dry-run --transfers 30 -P /mnt/tmpMedia/radarr data:enc/mnt/mega/app_data/complete_torrent_downloads 
+sudo rclone move --config /etc/rclone.conf --transfers 30 -P /mnt/tmpMedia/radarr data:enc/mnt/mega/app_data/complete_torrent_downloads
+sudo rclone move --config /etc/rclone.conf --transfers 30 -P /mnt/tmpMedia/sonarr data:enc/mnt/mega/app_data/complete_torrent_downloads 
