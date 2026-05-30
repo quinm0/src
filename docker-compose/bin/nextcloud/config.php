cde581b13ca538c8fb1239c7efac9cffb71c264e
quinm0 <jolly2633@tutamail.com>
2026-05-30 17:54:20 -0400

    AAAAA Nextcloud! - moved nextcloud configs into repo for ez tracking/backup - add nextcloud harp (NIGHTMARE)

---
+++ docker-compose/bin/nextcloud/config.php
@@ -0,0 +1,43 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'upgrade.disable-web' => true,
+  'instanceid' => 'ocrayh8zae4n',
+  'passwordsalt' => 'BBgq3y4ZkAtxO/19CoJHbj8Qk79W8j',
+  'secret' => 'xcf3d7wBERQPfSI+5UcXGqP0sIJv9zkVg31Qi1clgXQb/FRX',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost:8080',
+    1 => 'cloud.soupclown.com',
+  ),
+  'trusted_proxies' => ['100.98.69.57'],
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '33.0.4.1',
+  'overwrite.cli.url' => 'http://localhost:8080',
+  'dbname' => 'Unfounded2622',
+  'dbhost' => 'nextcloudDb',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'Wheat7331',
+  'dbpassword' => 'Gbxfd02Vz0oWm4JKUKfuHBsKE6dgrtx5',
+  'installed' => true,
+  'config_preset' => 7,
+  'defaultapp' => '',
+);
