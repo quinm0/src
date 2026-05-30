992901f15ba8728d0b8bfa8efca718c0396be574
quinm0 <jolly2633@tutamail.com>
2026-05-30 12:07:15 -0400

    More robust/reuseable backup script - fix mc server restart and profile values

--- backup.sh
+++ backup.sh
@@ -1,17 +1,59 @@
 #!/usr/bin/env bash
 
-echo "Stopping Services"
-pushd ./docker-compose
-docker compose --profile app down
-docker compose --profile download down
-docker compose --profile infra down
-popd
+ALL_COMPOSE_PROFILES=( 
+    "download" 
+    "app" 
+    "infra"
+)
+ENABLED_COMPOSE_PROFILES=(
+    "download"
+    "app"
+    "infra"
+)
 
-sudo restic -r /storage/restic --tag soupclownetc backup /etc/soupclown 
+# Method to run docker compose commands
+UPDATE_SERVICE_STATE(){
 
-echo "Restarting services"
-pushd ./docker-compose
-docker compose --profile app up -d
-docker compose --profile download up -d
-docker compose --profile infra up -d
-popd
+    # Default to up operation
+    OPERATION="up -d"
+    FINAL_COMPOSE_ARGS=""
+    
+    # Formulate profile args for up or down operation
+    # If running down command then use all profiles to ensure services are off before backup
+    # If runnin up only enable profiles listed ENABLED_COMPOSE_PROFILES
+    # 
+    for profile in "${ALL_COMPOSE_PROFILES[@]}"; do
+        if [[ $1 == "down" ]]; then
+            FINAL_COMPOSE_ARGS+=" --profile $profile"
+        else
+            # If current profile in ENABLED_COMPOSE_PROFILES then add to up compose args
+            if [[ "${ENABLED_COMPOSE_PROFILES[*]}" =~ (^|[^[:alpha:]])$profile([^[:alpha:]]|$) ]]; then
+                FINAL_COMPOSE_ARGS+=" --profile $profile"
+            fi
+        fi
+    done    
+
+    # Set operation to down if $1 == "down"
+    # otherwise start services
+    # 
+    if [[ $1 == "down" ]]; then
+        OPERATION="down"
+        echo "[DOCKER COMPOSE] Stopping Services"
+    else
+        echo "[DOCKER COMPOSE] Starting Services"
+    fi
+
+    # ACTUAL DOCKER COMPOSE OPERAION COMMAND
+    pushd ./docker-compose
+    docker compose $FINAL_COMPOSE_ARGS $OPERATION
+    popd
+}
+
+# MAIN TASKS
+BACKUP(){
+    UPDATE_SERVICE_STATE "down"
+    sudo restic -r /storage/restic --tag soupclownetc backup /etc/soupclown 
+    UPDATE_SERVICE_STATE "up"
+}
+
+BACKUP
