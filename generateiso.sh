16cd6c9b099c700a56eaa2ee2d90689f526052fd
quinm0 <jolly2633@tutamail.com>
2026-07-02 17:15:48 -0400

    disable deluge and work on macimage

--- generateiso.sh
+++ generateiso.sh
@@ -2,11 +2,7 @@
 
 # Make and store tempdir
 DIR=$(mktemp -d)
-
-# Copy iso config
-# Change this if you dare
-cp nix/installation-cd-graphical-gnome-macbook.nix
-
+OGDIR=$(pwd)
 
 pushd $DIR
 echo "DIR is ($DIR)"
@@ -15,10 +11,13 @@ echo "DIR is ($DIR)"
 git clone https://github.com/NixOS/nixpkgs.git
 cd nixpkgs/nixos
 
+# Copy iso config
+# Change this if you dare
+cp $OGDIR/nix/installation-cd-graphical-gnome-macbook.nix modules/installer/cd-dvd/installation-cd-graphical-gnome-macbook.nix 
+
 export NIXPKGS_ALLOW_UNFREE=1
 nix-build -A config.system.build.isoImage -I nixos-config=modules/installer/cd-dvd/installation-cd-graphical-gnome-macbook.nix default.nix
 
 
-echo "DONE."
-popd
+echo "DONE. (popd when done)"
 
--- nix/installation-cd-graphical-gnome-macbook.nix
+++ nix/installation-cd-graphical-gnome-macbook.nix
@@ -1,13 +1,25 @@
 { config, ... }:
 
 {
-  imports = [ ./installation-cd-graphical-gnome.nix ];
-
-  boot.initrd.kernelModules = [ "wl" ];
-
-  boot.kernelModules = [
-    "kvm-intel"
-    "wl"
+  imports = [ 
+    # ./installation-cd-graphical-gnome.nix
+    # ./installation-cd-minimal.nix
+    ./channel.nix
   ];
+
+  nixpkgs.config.allowUnfree = true;
+  boot.kernelModules = [ "kvm-intel" "wl"];
+  boot.initrd.kernelModules = [ "wl" ];
   boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+  boot.blacklistedKernelModules = [ "b43" "bcma" ];
+  boot.loader.grub.enable = false;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # the default governor constantly runs all cores on max frequency
+  # schedutil will run at a lower frequency and boost when needed
+  powerManagement.cpuFreqGovernor = "schedutil";
+
+  # install wpa_supplicant
+  networking.wireless.enable = true;
 }
