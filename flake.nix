46b78a62a6915fbec1dda4dc74c6e2b441be08bf
quinm0 <jolly2633@tutamail.com>
2026-08-22 15:32:56 -0400

    enable user service and syncthing

--- flake.nix
+++ flake.nix
@@ -80,6 +80,8 @@
             ./nix/hw/pi4hc.nix
             ./nix/shared/common.nix
             ./nix/shared/user-quin.nix
+            ./nix/shared/user-service.nix
+            ./nix/shared/syncthing.nix
             home-manager.nixosModules.home-manager
             nixos-raspberrypi.nixosModules.raspberry-pi-4.base
             nixos-raspberrypi.nixosModules.raspberry-pi-4.display-vc4
